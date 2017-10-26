2619036455d1ce58e40675233e243a8aa573b57e
Ingvix <arvi.sulin@hotmail.com>
2017-10-26 15:39:22 +0300

    Finnish translation

--- translations/harbour-followme-fi.ts
+++ translations/harbour-followme-fi.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../qml/components/DownloadQueue.qml" line="86"/>
         <source>Chapter</source>
-        <translation type="unfinished"></translation>
+        <translation>Luku</translation>
     </message>
 </context>
 <context>
@@ -14,38 +14,38 @@
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="37"/>
         <source>Chapter</source>
-        <translation type="unfinished"></translation>
+        <translation>Luku</translation>
     </message>
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="109"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Päivitä</translation>
     </message>
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="121"/>
         <source>Jump To</source>
-        <translation type="unfinished"></translation>
+        <translation>Siirry lukuun</translation>
     </message>
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="124"/>
         <source>Jump to chapter</source>
-        <translation type="unfinished"></translation>
+        <translation>Siirry lukuun</translation>
     </message>
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="126"/>
         <source>chapter</source>
-        <translation type="unfinished"></translation>
+        <translation>Luku</translation>
     </message>
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="137"/>
         <location filename="../qml/pages/EntryPage.qml" line="150"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Seuraava luku</translation>
     </message>
     <message>
         <location filename="../qml/pages/EntryPage.qml" line="142"/>
         <source>Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Edellinen luku</translation>
     </message>
 </context>
 <context>
@@ -53,64 +53,64 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="46"/>
         <source>FollowMe</source>
-        <translation type="unfinished"></translation>
+        <translation>Followme</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="71"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="86"/>
         <source>Stop downloading</source>
-        <translation type="unfinished"></translation>
+        <translation>Lopeta lataus</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="90"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation>Selaa</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="94"/>
         <location filename="../qml/pages/MainPage.qml" line="97"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Hae</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="98"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nimi</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="104"/>
         <location filename="../qml/pages/MainPage.qml" line="236"/>
         <source>Check updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Tarkista päivitykset</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="249"/>
         <source>Download some chapters</source>
-        <translation type="unfinished"></translation>
+        <translation>Lataa lukuja</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="261"/>
         <source>Download until chapter</source>
-        <translation type="unfinished"></translation>
+        <translation>Lataa lukuun</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="263"/>
         <source>chapter</source>
-        <translation type="unfinished"></translation>
+        <translation>Luku</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="283"/>
         <source>Cleanup read chapters</source>
-        <translation type="unfinished"></translation>
+        <translation>Poista luetut luvut</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="310"/>
         <source>Stop following</source>
-        <translation type="unfinished"></translation>
+        <translation>Lopeta seuraus</translation>
     </message>
 </context>
 <context>
@@ -118,7 +118,7 @@
     <message>
         <location filename="../qml/pages/ProviderPage.qml" line="45"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Päivitä</translation>
     </message>
 </context>
 <context>
@@ -127,7 +127,7 @@
         <location filename="../qml/pages/SearchDialog.qml" line="19"/>
         <location filename="../qml/pages/SearchDialog.qml" line="25"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Hae</translation>
     </message>
 </context>
 <context>
@@ -135,7 +135,7 @@
     <message>
         <location filename="../qml/pages/SearchPage.qml" line="31"/>
         <source>Search for </source>
-        <translation type="unfinished"></translation>
+        <translation>Hae</translation>
     </message>
 </context>
 <context>
@@ -143,17 +143,17 @@
     <message>
         <location filename="../qml/pages/SettingsDialog.qml" line="14"/>
         <source>FollowMe settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Asetukset</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsDialog.qml" line="20"/>
         <source>This data path will be used to store the images as well as the tracking data of all the followed entries. Keep in mind that if you change the data path after using the application, you will have to move the data manually.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kuvat sekä etenemistiedot tallennetaan tähän polkuun. Ota huomioon, että polkua muutettaessa sovelluksen käytön jälkeen on sovellustiedot siirrettävä manuaalisesti uuteen polkuun.</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsDialog.qml" line="28"/>
         <source>Data path</source>
-        <translation type="unfinished"></translation>
+        <translation>Polku</translation>
     </message>
 </context>
 </TS>
